63e96ccdef920bb319026f2e2c8a89ed104ec13c
Riya Mathur <51375575+Blitzcoder01@users.noreply.github.com>
2021-06-29 21:42:37 +0530

    Create 1470Shuffle_theArray.java

---
+++ 1470Shuffle_theArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] arr= new int[nums.length];
+        int c=0;
+        for(int i=0; i<nums.length-1; i=i+2){
+            arr[i]= nums[c];
+            arr[i+1]= nums[n+c];
+            c++;
+            
+        }
+        return arr;
+    }
+}
